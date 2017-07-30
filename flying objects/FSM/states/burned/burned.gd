77b06c2477760736677a19daf1f912ccca44575f
Daniel Lewan <vision360.daniel@gmail.com>
2017-07-30 21:54:33 +0200

    add animation when burning stuff

--- flying objects/FSM/states/burned/burned.gd
+++ flying objects/FSM/states/burned/burned.gd
@@ -25,6 +25,7 @@ func stateInit(inParam1=null,inParam2=null,inParam3=null,inParam4=null, inParam5
 #when entering state, usually you will want to reset internal state here somehow
 func enter(fromStateID=null, fromTransitionID=null, inArg0=null,inArg1=null, inArg2=null):
 	get_tree().get_root().get_node("/root/game/home").speed += value
+	get_tree().get_root().get_node("/root/game/home/burn stuff").play("burn stuff")
 #	logicRoot.call_deferred("queue_free")
 	logicRoot.queue_free()
 
