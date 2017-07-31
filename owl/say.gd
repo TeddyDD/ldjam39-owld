5b27ad896b18d93c9ba88d2323ae821f046d29e1
Daniel Lewan <vision360.daniel@gmail.com>
2017-07-31 16:07:20 +0200

    add taking system

---
+++ owl/say.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+onready var anim = get_node("AnimationPlayer")
+onready var text = get_node("text")
+onready var timer = get_node("timeout")
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+	
+func say(what, timeout=3):
+	text.set_text(what)
+	anim.play("show")
+	timer.set_wait_time(timeout)
+	
+func start_timer():
+	timer.start()
+
+func _on_timeout_timeout():
+	anim.play("hide")
