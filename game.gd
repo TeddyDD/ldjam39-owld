1f50ede5c89b35e9aeddb3d9d6d26f8bd4b14f5a
Daniel Lewan <vision360.daniel@gmail.com>
2017-07-31 14:57:45 +0200

    new bg

--- game.gd
+++ game.gd
@@ -14,15 +14,21 @@ func _process(delta):
 
 
 func _on_fuel_spawner_timeout():
-	var f = fuel.instance()
-	# spawn position
-	var p = Vector2(get_node("owl/spawner").get_global_pos()\
-	+ Vector2(rand_range(-300,300),0))
-#	prints(p)
-	# spawn velocity vector
-	var v = (get_node("owl").get_pos() - p) + Vector2(rand_range(-100,100), rand_range(-50,0))
-	f.velocity = v
-	add_child(f)
-	f.set_pos(p)
+	var dist = get_node("owl").get_pos().distance_to(get_node("home").get_pos())
+	prints(dist)
+	if dist > 500:
+		var f = fuel.instance()
+		# spawn position
+	#	var p = Vector2(get_node("owl/spawner").get_global_pos()\
+	#	+ Vector2(rand_range(-300,300),0))
+	
+		var p = Vector2( get_node("owl/spawner").get_pos().x + rand_range(-600,600),\
+		                 get_node("owl/spawner").get_global_mouse_pos().y - 700)
+	
+		# spawn velocity vector
+		var v = (get_node("owl").get_pos() - p).normalized() * rand_range(0,300) + Vector2(rand_range(-500,500), rand_range(-50,0))
+		f.velocity = v
+		add_child(f)
+		f.set_pos(p)
 	
 	get_node("fuel spawner").start()
